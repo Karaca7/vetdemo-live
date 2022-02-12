206f1c74430cd45157ae937a2a5a914d769f2f7b
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-12 14:05:48 +0300

    Create "Pet,PetOwner" models , link their relation
    Defined two models, pet and petowner. One to many relationships were
    made between the petowner and the pet, and thirdly, one to many
    relationships were created between the veterinarian, that is, the user
    model, and the pet and pet owner.

--- userpage/admin.py
+++ userpage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from userpage.models import Pet,PetOwner
 
 # Register your models here.
+
+admin.site.register(Pet)
+admin.site.register(PetOwner)
--- userpage/models.py
+++ userpage/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+from django.contrib.auth.models import User
+
+class PetOwner(models.Model):
+  
+    Name=models.CharField(max_length=20)
+    Surname=models.CharField(max_length=20)
+    Address=models.TextField()
+    Phone=models.CharField(max_length=11)
+    Email=models.CharField(max_length=100)
+    Vet=models.ForeignKey(User,default="",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
+
+
+class Pet(models.Model):
+    
+
+    PetName=models.CharField(max_length=20)
+    PetAge=models.CharField(max_length=3)
+    PetType=models.CharField(max_length=20)
+    PetExplan=models.TextField()
+    PetO=models.ForeignKey(PetOwner, default="",on_delete=models.CASCADE)
+    Vet=models.ForeignKey(User,default="",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.PetName
