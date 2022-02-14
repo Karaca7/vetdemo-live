3dcf3f95afd0081d82f6ab624acabe0a503a2253
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-15 02:09:24 +0300

    Fixed Set the STATIC_ROOT variable in settings

--- vet/settings.py
+++ vet/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATİC_ROOT=BASE_DIR/'staticfiles'
+STATIC_ROOT=BASE_DIR/'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
